5d79fc05b36b9cd5dd8058a88c5ac39de5d8589e
Jonathan W <jwilliams12117@gmail.com>
2022-05-14 19:26:28 -0700

    Set up projects page and linked to queries file in utils

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Homepage from './pages/Homepage';
+import Projects from "./pages/Projects";
 import NoMatch from './pages/NoMatch';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -34,6 +34,10 @@ function App() {
               <Route
                 path="/"
                 element={<Homepage />}
+              />
+              <Route 
+                path="/projects"
+                element={<Projects />}
               />
               <Route
                 path="*"
---
+++ client/src/pages/Projects.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_EVENTS } from "../utils/queries";
+
+
+const Projects = () => {
+    // useQuery hook grabs the data from the query and shows loading while the async function works
+    const {loading, data} = useQuery(QUERY_EVENTS);
+
+    // Optional chaining to check if object exists and assign to an empty array if not
+    const events = data?.events || [];
+    
+    return (
+        <div>
+            <h1>Upcoming Projects</h1>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
