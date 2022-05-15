150439a788eaad98992b83051787e89494397c78
Jonathan W <jwilliams12117@gmail.com>
2022-05-15 10:32:27 -0700

    Implemented logout functionality

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,22 +4,38 @@ import Auth from "../../utils/auth";
 
 const Header = () => {
 
+    // Logout function
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
     return (
         <header>
             <nav>
-                
+                {/* Conditionally Render navbar items based on whether the user
+                is currently logged in, which is evaluated with this Auth.loggedIn method
+                and a ternary operator */}
+                {Auth.loggedIn() ? (
+                    <>
+                        <button>
+                            <Link to="/">Home</Link>
+                        </button>
+                        <button onClick={logout}>
+                            Log Out
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <button>
+                            <Link to="/login">Log In</Link>
+                        </button>
+                        <button>
+                            <Link to="/signup">Sign Up</Link>
+                        </button>
+                    </>
+                )}
             </nav>
-            <div>
-                <button>
-                    <Link to="/login">Log In</Link>
-                </button>
-                <button>
-                    <Link to="/signup">Sign Up</Link>
-                </button>
-                <button>
-                    <Link to="/">Log Out</Link>
-                </button>
-            </div>
         </header>
     )
 }
