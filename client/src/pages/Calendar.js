dd2e60288e429ef6abdd063e047c53212b57a633
Jonathan W <jwilliams12117@gmail.com>
2022-05-29 22:23:15 -0700

    Added Heroku deployment

---
+++ client/src/pages/Calendar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Calendar = () => {
+
+    // Queries to grab user data and authenticate
+
+    // Queries to grab event data and tasks for each user
+
+    // Function to display to the calendar
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Calendar;
